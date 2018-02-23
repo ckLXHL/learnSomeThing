d48c084f50d18b5a0f6ef95f9d717098eb2ec8c7
caokai <caokai@didichuxing.com>
2018-02-23 17:58:09 +0800

    添加刷题main

---
+++ train_guide/train_guide/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  train_guide
+//
+//  Created by didi on 2018/2/23.
+//  Copyright © 2018年 didi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
