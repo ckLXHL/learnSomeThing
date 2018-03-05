e918966005e0a7371122b5ebf60f37bd6148a09a
caokai <caokai@didichuxing.com>
2018-03-05 22:36:05 +0800

    补充ss安装和开启

--- myinit/init.sh
+++ myinit/init.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-pip install shadowsocks -y
-sslocal -s 35.194.219.96 -p 8989 -k "123456" -m aes-256-gcm
+yes | pip install shadowsocks
+sslocal -s 35.194.219.96 -p 8989 -k "123456" -m aes-256-gcm >/dev/null 2>&1 &
+proxy_on
 
 function proxy_on() {
      export no_proxy="localhost,127.0.0.1,localaddress,.localdomain.com"
