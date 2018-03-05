005a71c94cf92dc4ccde1a033daf2771d9338e90
caokai <caokai@didichuxing.com>
2018-03-05 22:42:02 +0800

    添加jupyter启动代码

--- myinit/init.sh
+++ myinit/init.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
+
 yes | pip install shadowsocks
+
 sslocal -s 35.194.219.96 -p 8989 -k "123456" -m aes-256-gcm >/dev/null 2>&1 &
+
 proxy_on
+
+jupyter notebook >/dev/null 2>&1 &
 
 function proxy_on() {
      export no_proxy="localhost,127.0.0.1,localaddress,.localdomain.com"
